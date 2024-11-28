d696fcd0657fe4cb044200670e04ac4ca84884bf
Mario Alexander Muñoz Barboza <munozbarbozamarioalexander@gmail.com>
2024-11-28 10:47:21 -0500

    Demostracion de hooks-formato-codigo

--- DemoHooks/Program.cs
+++ DemoHooks/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola, mundo!");
+Console.WriteLine("Prueba de formateo de codigo");
+int edad = 34;
+if (edad >= 18)
+{
+    Console.WriteLine("Es mayor de edad");
+           }
+else
+{
+    Console.WriteLine("Es menos de edad");
+}
